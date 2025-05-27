068b520f1af46b6555726a7182ccb4e47539dc35
mabindajoash <mabindajoash@gmail.com>
2025-05-27 16:35:13 +0000

    task 4

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -5,19 +5,24 @@ from typing import List, TypeVar
 
 User = TypeVar('User')
 
+
 class Auth:
     """Class to be used for authentication"""
-
-
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """public method returns false"""
-        return False
+        if path is None:
+            return True
+        if not excluded_paths:
+            return True
+        normalized_path = path if path.endswith('/') else path + '/'
 
+        if normalized_path in excluded_paths:
+            return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """authoriztion header"""
         return None
-
 
     def current_user(self, request=None) -> User:
         """current user"""
